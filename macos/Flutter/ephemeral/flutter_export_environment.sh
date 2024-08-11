599e6853bede072a3889e2956342c248fd67f589
Tristan <tristanshaw000@gmail.com>
2024-08-11 00:51:34 -0400

    icons for web and desktop operating systems!

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.24.0/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tristanshaw/Home/breeze"
+export "FLUTTER_ROOT=/home/tristans/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/tristans/Code/breeze"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
-export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/tristanshaw/Home/breeze/.dart_tool/package_config.json"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
